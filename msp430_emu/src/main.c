d61b5fb30bd7d4f888ce70a4170a5d338c5afdda
Mateusz Wojeński <mateusz710@gmail.com>
2015-10-30 12:47:49 +0100

    Wrzucam to z repo od Pawla 30.10.2015

---
+++ msp430_emu/src/main.c
@@ -0,0 +1,19 @@
+int main(void)
+{
+	// sprawdzenie warunkow poczatkowych (uruchomienie programu z wlasciwymi opcjami)
+	// sprawdzenie istnienia plikow do wczytania
+	// plik konfiguracji
+	// plik z programem
+	// plik z danymi programu
+	
+	// start cpu
+	// inicjalizacja cpu
+	
+	// start glownej petli wykonujacej emulacje dzialania cpu
+	
+	// while (1)
+	// do instructions
+	// 
+
+	return 0;
+}
